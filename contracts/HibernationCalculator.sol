facb14fcca6309d706aff8433d0697a8ea2fc704
rarescosmin <rarescosmin97@gmail.com>
2021-12-09 16:59:24 +0200

    added animalCount and animal map

--- contracts/HibernationCalculator.sol
+++ contracts/HibernationCalculator.sol
@@ -3,4 +3,28 @@ pragma solidity ^0.8.10;
 
 contract HibernationCalculator {
     uint public animalCount = 0;
+
+    struct HibernationInformation {
+        string uuid;
+        uint length;
+        uint weight;
+        uint result;
+        uint min;
+        uint avg;
+        uint max;
+    }
+
+    mapping(string => HibernationInformation) public animals;
+
+    function addAnimal(
+        string memory uuid,
+        uint length,
+        uint weight,
+        uint result,
+        uint min,
+        uint avg,
+        uint max
+    ) public {
+        animalCount ++;
+    }
 }
